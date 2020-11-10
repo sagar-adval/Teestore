3ac4c5b42a01ccb20d374d225b818f84e76d1cde
Sagar Adval <sagaradval@gmail.com>
2020-11-10 16:59:44 +0530

    Added backend base url(localhost)

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles.css";
+import {API} from "../backend"
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-white">Hello front end</h1>
+      <h1 className="text-white">Hello front end {API}</h1>
     </div>
   );
 }
